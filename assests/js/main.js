c63043306f44e1b681e463d6330c888403bac569
Sinh Kavya <kavya.singh@shunyavkash.com>
2025-04-24 17:41:50 +0530

    silder-erro

--- assests/js/main.js
+++ assests/js/main.js
@@ -135,30 +135,6 @@ $(document).ready(function () {
 
 // swiper slider script
 
-var brandLogo = new Splide(".brand-logo", {
-  type: "loop",
-  focus: "center",
-  perMove: 1,
-  perPage: 5,
-  gap: "20px",
-  arrows: false,
-  pagination: false,
-  autoplay: {
-    speed: 1,
-  },
-  breakpoints: {
-    768: {
-      perPage: 4,
-      gap: "30px",
-    },
-    640: {
-      perPage: 3,
-      gap: "30px",
-    },
-  },
-});
-brandLogo.mount(window.splide.Extensions);
-
 var socialImg = new Splide(".social-img", {
   type: "loop",
   drag: "free",
@@ -187,6 +163,30 @@ var socialImg = new Splide(".social-img", {
   },
 });
 socialImg.mount(window.splide.Extensions);
+
+var brandLogo = new Splide(".brand-logo", {
+  type: "loop",
+  focus: "center",
+  perMove: 1,
+  perPage: 5,
+  gap: "20px",
+  arrows: false,
+  pagination: false,
+  autoplay: {
+    speed: 1,
+  },
+  breakpoints: {
+    768: {
+      perPage: 4,
+      gap: "30px",
+    },
+    640: {
+      perPage: 3,
+      gap: "30px",
+    },
+  },
+});
+brandLogo.mount(window.splide.Extensions);
 
 // In your Javascript (external .js resource or <script> tag)
 var autoScroll = new Splide(".auto-scroll-sider", {
